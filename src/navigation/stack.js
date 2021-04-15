fd2e7929534a234b1e35d7b5fb3b5c084c4c15bc
Alan Ribeiro <alanribeiro@MacBook-Pro-de-Alan.local>
2021-04-15 15:52:18 -0300

    nHome listItems

--- src/navigation/stack.js
+++ src/navigation/stack.js
@@ -1,22 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from "react-native";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from 'react-navigation-stack';
 
 import HomeScreen from "../screens/HomeScreen";
+import newQuestionScreen from "../screens/NewQuestion"
+
+import { Icon } from 'react-native-elements';
 
 const AppNavigator = createStackNavigator(
-    {
-      home: {
-        screen: HomeScreen,
-        navigationOptions: {
-          header: null,
-        },
-      }
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null, // Will hide header for all screens of current stack navigator,
+      }),
     },
-    {
-      initialRouteName: "home",
+    newQuestion: {
+      screen: newQuestionScreen,
+      navigationOptions: ({ navigation, route }) => ({
+        title: 'Create New Question',
+
+        headerStyle: {
+          backgroundColor: '#535CF7',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+
+
+        headerLeft: (
+          <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingHorizontal: 10 }}>
+            <Icon
+              name='arrow-left'
+              type='font-awesome'
+              color='#fff'
+              size={20}
+              iconStyle={{ marginLeft: 2, marginTop: -1 }}
+            />
+          </TouchableOpacity>
+        ),
+      }),
     }
-  )
-  
-  const Container = createAppContainer(AppNavigator)
-  export default Container
+  },
+  {
+    initialRouteName: "HomeScreen",
+  }
+)
+
+const Container = createAppContainer(AppNavigator)
+export default Container
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
+import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
 
-
-import { Text, StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import { Button } from 'react-native-elements'
 import ListView from '../components/HomeListView/ListView'
 
-const HomeScreen = () => {
+export default ({ props, route, navigation }) => {
 
     return (
-        <SafeAreaView style={styles.container}>
-                <ListView />
-        </SafeAreaView>
+        <Fragment>
+            <SafeAreaView style={{ flex: 0, backgroundColor: '#535CF7' }} />
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+                <StatusBar barStyle="light-content" backgroundColor="#535CF7" />
+                <ListView navigation={navigation} />
+                <View style={styles.containerButtons}>
+                    <Button
+                        Outline
+                        title="CREATE NEW QUESTION"
+                        containerStyle={styles.buttonOutStyle}
+                        buttonStyle={styles.buttonStyle}
+                        onPress={() => navigation.navigate('newQuestion')}
+                    />
+                </View>
+            </SafeAreaView>
+        </Fragment>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        flex: 1
+    containerButtons: {
+        backgroundColor: '#fff'
+    },
+    buttonOutStyle: {
+        margin: 10
+    },
+    buttonStyle: {
+        backgroundColor: '#535CF7',
+        borderRadius: 10,
+        padding: 15
     }
 });
-
-export default HomeScreen;
